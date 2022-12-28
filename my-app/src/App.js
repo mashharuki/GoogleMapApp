55e6c662bacb80284c7709508d0e7508553eda78
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-12-28 14:43:32 +0900

    修正

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import {useState, useCallback} from 'react';
 
+const containerStyle = {
+  width: '600px',
+  height: '600px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+/**
+ * App component
+ * @returns 
+ */
 function App() {
+  const [map, setMap] = useState(null);
+
+  /**
+   * useJsApiLoader function
+   */
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
+  });
+
+  /**
+   * onLoad function
+   */
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  /**
+   * onUnmount function
+   */
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+        ) : (
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </>
+        )}
       </header>
     </div>
   );
